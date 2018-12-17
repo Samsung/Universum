e43ca2fef88c570db1e3fcf56b27c15f8ab07cf2
Anhelina Shevchuk <an.shevchuk@samsung.com>
2018-12-17 13:01:03 +0200

    fix(code_report): fixed wrong result stdout for code_report
    Change-Id: I00a1496b57897ba43a57075457c5f542f8f2fac3

--- _universum/modules/code_report_collector.py
+++ _universum/modules/code_report_collector.py
@@ -59,7 +59,10 @@ class CodeReportCollector(ProjectDirectory):
         reports = glob.glob(self.report_path + "/*.json")
         for report_file in reports:
             with open(report_file, "r") as f:
-                report = json.loads(f.read())
+                text = f.read()
+                report = ""
+                if text:
+                    report = json.loads(text)
 
             json_file = self.artifacts.create_text_file("Static_analysis_report.json")
             json_file.write(json.dumps(report, indent=4))
@@ -72,6 +75,7 @@ class CodeReportCollector(ProjectDirectory):
                 text = unicode(len(report)) + " issues"
                 self.out.log_stderr("Found " + text)
                 self.out.report_build_status(os.path.splitext(os.path.basename(report_file))[0] + ": " + text)
-
-        if not reports:  # e.g. required module is not installed (pylint, load-plugins for pylintrc)
-            self.out.log("Issues not found.")
+            elif report == []:
+                self.out.log("Issues not found.")
+            else:  # if nothing was written to file
+                self.stderr.write("There are no results in code report file. Something went wrong.")
