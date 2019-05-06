9a4622ae285f7f2503dcc3f685ac9ec88241186b
Ivan Keliukh <i.keliukh@samsung.com>
2019-05-06 17:32:36 +0300

    feat(test): add parameter checks for jenkins

--- tests/test_argument_check.py
+++ tests/test_argument_check.py
@@ -8,7 +8,7 @@ from _universum.lib.module_arguments import IncorrectParameterError
 from . import utils
 
 
-def create_settings(test_type, vcs_type):
+def create_settings(test_type, vcs_type, server_type="tc"):
     settings = utils.create_empty_settings(test_type)
     settings.Output.type = "term"
     settings.ProjectDirectory.project_root = "project_root"
@@ -16,20 +16,26 @@ def create_settings(test_type, vcs_type):
 
     if test_type == "poll":
         settings.Poll.db_file = "p4poll.json"
-        settings.JenkinsServer.trigger_url = "https://localhost/?cl=%s"
-        settings.AutomationServer.type = "jenkins"
     elif test_type == "submit":
         settings.Submit.commit_message = "Test CL"
     elif test_type == "main":
         settings.Launcher.config_path = "configs.py"
         settings.ArtifactCollector.artifact_dir = "artifacts"
 
-        settings.AutomationServer.type = "tc"
-        settings.TeamcityServer.server_url = "https://teamcity"
-        settings.TeamcityServer.build_id = "BuildId_167"
-        settings.TeamcityServer.configuration_id = "TestConfig"
-        settings.TeamcityServer.user_id = "TeamCityUser"
-        settings.TeamcityServer.passwd = "TeamCityPassword"
+    if test_type == "poll" or test_type == "main":
+        settings.AutomationServer.type = server_type
+
+        if server_type == "tc":
+            settings.TeamcityServer.server_url = "https://teamcity"
+            settings.TeamcityServer.build_id = "BuildId_167"
+            settings.TeamcityServer.configuration_id = "TestConfig"
+            settings.TeamcityServer.user_id = "TeamCityUser"
+            settings.TeamcityServer.passwd = "TeamCityPassword"
+        elif server_type == "jenkins":
+            if test_type == "main":
+                settings.JenkinsServerForHostingBuild.build_url = "https://jenkins"
+            else:
+                settings.JenkinsServerForTrigger.trigger_url = "https://localhost/?cl=%s"
 
     if vcs_type == "git":
         settings.GitVcs.repo = "git://127.0.0.1"
@@ -75,45 +81,48 @@ def assert_incorrect_parameter(settings, match):
 missing_params = []
 
 
-def param(test_type, module, field, vcs_type="*", error_match=None):
+def param(test_type, module, field, vcs_type="*", error_match=None, server_type="tc"):
     # type: (str, str, str, Union[str, List[str]], str) -> None
     global missing_params
 
     if isinstance(vcs_type, list):
         for vcs in vcs_type:
-            param(test_type, module, field, vcs, error_match)
+            param(test_type, module, field, vcs, error_match, server_type)
         return
 
     if vcs_type == "*":
-        param(test_type, module, field, ["p4", "git", "none"], error_match)
+        param(test_type, module, field, ["p4", "git", "none"], error_match, server_type)
         return
 
     if error_match is None:
         error_match = field
 
-    test_id = test_type + "-" + vcs_type + "-"+ module + "." + field
-    missing_params.append(pytest.param(test_type, module, field, vcs_type, error_match, id=test_id))
+    test_id = test_type + "-" + vcs_type + "-" + module + "." + field
+    missing_params.append(pytest.param(test_type, module, field, vcs_type, error_match, server_type, id=test_id))
 
 
 # pylint: disable = bad-whitespace
-param("main",   "Launcher",             "config_path")
-param("submit", "Submit",               "commit_message",  vcs_type=["p4", "git"])
-param("submit", "PerforceSubmitVcs",    "client",          vcs_type="p4")
-param("main",   "PerforceMainVcs",      "client",          vcs_type="p4")
-param("main",   "LocalMainVcs",         "source_dir",      vcs_type="none")
-param("submit", "GitSubmitVcs",         "user",            vcs_type="git")
-param("submit", "GitSubmitVcs",         "email",           vcs_type="git")
-param("main",   "TeamcityServer",       "build_id")
-param("main",   "TeamcityServer",       "configuration_id")
-param("main",   "TeamcityServer",       "server_url",      error_match="TEAMCITY_SERVER")
-param("main",   "TeamcityServer",       "user_id",         error_match="TC_USER")
-param("main",   "TeamcityServer",       "passwd",          error_match="TC_PASSWD")
+param("main",   "Launcher",                     "config_path")
+param("submit", "Submit",                       "commit_message",  vcs_type=["p4", "git"])
+param("submit", "PerforceSubmitVcs",            "client",          vcs_type="p4")
+param("main",   "PerforceMainVcs",              "client",          vcs_type="p4")
+param("main",   "LocalMainVcs",                 "source_dir",      vcs_type="none")
+param("submit", "GitSubmitVcs",                 "user",            vcs_type="git")
+param("submit", "GitSubmitVcs",                 "email",           vcs_type="git")
+param("main",   "TeamcityServer",               "build_id")
+param("main",   "TeamcityServer",               "configuration_id")
+param("main",   "TeamcityServer",               "server_url",      error_match="TEAMCITY_SERVER")
+param("main",   "TeamcityServer",               "user_id",         error_match="TC_USER")
+param("main",   "TeamcityServer",               "passwd",          error_match="TC_PASSWD")
+param("main",   "JenkinsServerForHostingBuild", "build_url",       server_type="jenkins")
+param("poll",   "JenkinsServerForTrigger",      "trigger_url",     server_type="jenkins", vcs_type=["p4", "git"], error_match="url for triggering build")
 # pylint: enable = bad-whitespace
 
+
 @parametrize_unset()
-@pytest.mark.parametrize("test_type, module, field, vcs_type, error_match", missing_params)
-def test_missing_params(unset, test_type, module, field, vcs_type, error_match):
-    settings = create_settings(test_type, vcs_type)
+@pytest.mark.parametrize("test_type, module, field, vcs_type, error_match, server_type", missing_params)
+def test_missing_params(unset, test_type, module, field, vcs_type, error_match, server_type):
+    settings = create_settings(test_type, vcs_type, server_type)
     unset(settings, module, field)
 
     assert_incorrect_parameter(settings, "(?i)" + error_match)
