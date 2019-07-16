d6565eea9a8e906d79c59a39d8253f71b84d1ecb
Kateryna Dovgan <k.dovgan@samsung.com>
2019-07-16 14:31:58 +0300

    fix: add both log start and log end identifiers

--- universum.py
+++ universum.py
@@ -41,7 +41,7 @@ def define_arguments():
 def run(settings):
     result = 0
     main_module = construct_component(settings.main_class, settings)
-    main_module.out.log("Running {} {}".format(__title__, __version__))
+    main_module.out.log("Execution started. {} {}".format(__title__, __version__))
 
     finalized = False
 
@@ -69,6 +69,7 @@ def run(settings):
     signal.signal(signal.SIGHUP, finalize)
     signal.signal(signal.SIGINT, finalize)
 
+    main_module.out.log("Execution finished")
     return result
 
 
