060294f7bf9608d20ee3bba73943e32363cc2d24
miltolstoy <miltolstoy@gmail.com>
2019-10-17 09:48:44 +0300

    test(jenkins_plugin): add Java tests for log start, end, break

---
+++ universum_log_collapser/src/test/java/io/jenkins/plugins/universum_log_collapser/LogActiveTest.java
@@ -0,0 +1,398 @@
+package io.jenkins.plugins.universum_log_collapser;
+
+import org.junit.Test;
+
+import hudson.MarkupText;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+import static org.junit.runners.Parameterized.Parameter;
+import static org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Enclosed.class)
+public class LogActiveTest {
+
+    private static final String logStartLine = "==> Universum 1.2.3 started execution";
+    private static final String logEndLine = "==> Universum 1.2.3 finished execution";
+
+    public static class nonParametrizedTest {
+
+        @Test
+        public void logStartStop() {
+            String[] in = new String[] {
+                    "1. Step name",
+                    " |   step data",
+                    " └ [Success]",
+                    logStartLine,
+                    "1. Step name",
+                    " |   step data",
+                    " └ [Success]",
+                    logEndLine,
+                    "1. Step name",
+                    " |   step data",
+                    " └ [Success]",
+                    logStartLine,
+                    "1. Step name",
+                    " |   step data",
+                    " └ [Success]",
+            };
+            String[] out = new String[] {
+                    "1. Step name",
+                    " |   step data",
+                    " └ [Success]",
+                    replaceWithHtmlEntities(logStartLine),
+                    wrapSectionStart("1. Step name", 1),
+                    wrapSectionContent(" |   step data"),
+                    wrapSectionEnd(" └ [Success]"),
+                    replaceWithHtmlEntities(logEndLine),
+                    "1. Step name",
+                    " |   step data",
+                    " └ [Success]",
+                    replaceWithHtmlEntities(logStartLine),
+                    wrapSectionStart("1. Step name", 2),
+                    wrapSectionContent(" |   step data"),
+                    wrapSectionEnd(" └ [Success]"),
+            };
+            checkAnnotation(in, out);
+        }
+
+        @Test
+        public void logNotBroken() {
+            String[] in = new String[] {
+                    logStartLine,
+                    "1. Step name",
+                    " |   step data",
+                    " └ [Success]",
+                    "some random line",
+                    "1. Step name",
+                    " |   step data",
+                    " └ [Success]"
+            };
+            String[] out = new String[] {
+                    replaceWithHtmlEntities(logStartLine),
+                    wrapSectionStart("1. Step name", 1),
+                    wrapSectionContent(" |   step data"),
+                    wrapSectionEnd(" └ [Success]"),
+                    "some random line",
+                    wrapSectionStart("1. Step name", 2),
+                    wrapSectionContent(" |   step data"),
+                    wrapSectionEnd(" └ [Success]")
+            };
+            checkAnnotation(in, out);
+        }
+
+        @Test
+        public void logBroken() {
+            String[] in = new String[] {
+                    logStartLine,
+                    "1. Step name",
+                    " |   step data",
+                    "some random line",
+                    " └ [Success]",
+                    "1. Step name",
+                    " |   step data",
+                    " └ [Success]"
+            };
+            String[] out = new String[] {
+                    replaceWithHtmlEntities(logStartLine),
+                    wrapSectionStart("1. Step name", 1),
+                    wrapSectionContent(" |   step data"),
+                    "some random line",
+                    " └ [Success]",
+                    "1. Step name",
+                    " |   step data",
+                    " └ [Success]"
+            };
+            checkAnnotation(in, out);
+        }
+    }
+
+    @RunWith(Parameterized.class)
+    public static class LogStartPositiveTest {
+
+        @Parameter
+        public String line;
+
+        @Parameters(name="{0}")
+        public static Iterable<String> testData() {
+            return Arrays.asList(
+                    "==> Universum 1.2.3 started execution",
+                    "==> Universum 0.0.0 started execution",
+                    "==> Universum 1234.234521.952 started execution"
+            );
+        }
+
+        @Test
+        public void test() {
+            String[] in = new String[]{
+                    "1. Step name",
+                    " |   step data",
+                    " └ [Success]",
+                    line,
+                    "1. Step name",
+                    " |   step data",
+                    " └ [Success]",
+            };
+            String[] out = new String[] {
+                    "1. Step name",
+                    " |   step data",
+                    " └ [Success]",
+                    replaceWithHtmlEntities(line),
+                    wrapSectionStart("1. Step name", 1),
+                    wrapSectionContent(" |   step data"),
+                    wrapSectionEnd(" └ [Success]"),
+            };
+            checkAnnotation(in, out);
+        }
+    }
+
+    @RunWith(Parameterized.class)
+    public static class LogStartNegativeTest {
+
+        @Parameter
+        public String line;
+
+        @Parameters(name="{0}")
+        public static Iterable<String> testData() {
+            return Arrays.asList(
+                    "==> Universum 1.2.3 finished execution",
+                    " ==> Universum 1.2.3 started execution",
+                    "=< Universum 1.2.3 started execution",
+                    "==> universum 1.2.3 started execution",
+                    "==> Universum 1.2 started execution",
+                    "==> Universum 1.2 started execution now"
+            );
+        }
+
+        @Test
+        public void test() {
+            String[] in = new String[]{
+                    "1. Step name",
+                    " |   step data",
+                    " └ [Success]",
+                    line,
+                    "1. Step name",
+                    " |   step data",
+                    " └ [Success]",
+            };
+            String[] out = new String[] {
+                    "1. Step name",
+                    " |   step data",
+                    " └ [Success]",
+                    replaceWithHtmlEntities(line),
+                    "1. Step name",
+                    " |   step data",
+                    " └ [Success]",
+            };
+            checkAnnotation(in, out);
+        }
+    }
+
+    @RunWith(Parameterized.class)
+    public static class LogEndPositiveTest {
+
+        @Parameter
+        public String line;
+
+        @Parameters(name="{0}")
+        public static Iterable<String> testData() {
+            return Arrays.asList(
+                    "==> Universum 1.2.3 finished execution",
+                    "==> Universum 0.0.0 finished execution",
+                    "==> Universum 1234.234521.952 finished execution"
+            );
+        }
+
+        @Test
+        public void test() {
+            String[] in = new String[]{
+                    "1. Step name",
+                    " |   step data",
+                    " └ [Success]",
+                    logStartLine,
+                    "1. Step name",
+                    " |   step data",
+                    " └ [Success]",
+                    line,
+                    "1. Step name",
+                    " |   step data",
+                    " └ [Success]",
+            };
+            String[] out = new String[] {
+                    "1. Step name",
+                    " |   step data",
+                    " └ [Success]",
+                    replaceWithHtmlEntities(logStartLine),
+                    wrapSectionStart("1. Step name", 1),
+                    wrapSectionContent(" |   step data"),
+                    wrapSectionEnd(" └ [Success]"),
+                    replaceWithHtmlEntities(line),
+                    "1. Step name",
+                    " |   step data",
+                    " └ [Success]",
+            };
+            checkAnnotation(in, out);
+        }
+    }
+
+    @RunWith(Parameterized.class)
+    public static class LogEndNegativeTest {
+
+        @Parameter
+        public String line;
+
+        @Parameters(name="{0}")
+        public static Iterable<String> testData() {
+            return Arrays.asList(
+                    "==> Universum 1.2.3 started execution",
+                    " ==> Universum 1.2.3 finished execution",
+                    "=< Universum 1.2.3 finished execution",
+                    "==> universum 1.2.3 finished execution",
+                    "==> Universum 1.2 finished execution",
+                    "==> Universum 1.2 finished execution now"
+            );
+        }
+
+        @Test
+        public void test() {
+            String[] in = new String[]{
+                    "1. Step name",
+                    " |   step data",
+                    " └ [Success]",
+                    logStartLine,
+                    "1. Step name",
+                    " |   step data",
+                    " └ [Success]",
+                    line,
+                    "1. Step name",
+                    " |   step data",
+                    " └ [Success]",
+            };
+            String[] out = new String[] {
+                    "1. Step name",
+                    " |   step data",
+                    " └ [Success]",
+                    replaceWithHtmlEntities(logStartLine),
+                    wrapSectionStart("1. Step name", 1),
+                    wrapSectionContent(" |   step data"),
+                    wrapSectionEnd(" └ [Success]"),
+                    replaceWithHtmlEntities(line),
+                    wrapSectionStart("1. Step name", 2),
+                    wrapSectionContent(" |   step data"),
+                    wrapSectionEnd(" └ [Success]"),
+            };
+            checkAnnotation(in, out);
+        }
+    }
+
+    @RunWith(Parameterized.class)
+    public static class JobFinishedPositiveTest {
+
+        @Parameter
+        public String line;
+
+        @Parameters(name="{0}")
+        public static Iterable<String> testData() {
+            // https://javadoc.jenkins-ci.org/hudson/model/Result.html
+            return Arrays.asList(
+                    "Finished: SUCCESS",
+                    "Finished: ABORTED",
+                    "Finished: FAILURE",
+                    "Finished: NOT_BUILT",
+                    "Finished: UNSTABLE"
+            );
+        }
+
+        @Test
+        public void test() {
+            String[] in = new String[]{
+                    line,
+                    logStartLine,
+                    line
+            };
+            String finishColoringIframe = "<iframe onload=\"finishColoring()\" style=\"display:none\"></iframe>";
+            String[] out = new String[] {
+                    line + finishColoringIframe,
+                    replaceWithHtmlEntities(logStartLine),
+                    line + finishColoringIframe
+            };
+            checkAnnotation(in, out);
+        }
+    }
+
+    @RunWith(Parameterized.class)
+    public static class JobFinishedNegativeTest {
+
+        @Parameter
+        public String line;
+
+        @Parameters(name="{0}")
+        public static Iterable<String> testData() {
+            return Arrays.asList(
+                    " Finished: SUCCESS",
+                    "Finished: SUCCESS ",
+                    "Finished: ",
+                    "finish",
+                    "Finished: success"
+            );
+        }
+
+        @Test
+        public void test() {
+            String[] in = new String[]{
+                    logStartLine,
+                    line
+            };
+            String[] out = new String[] {
+                    replaceWithHtmlEntities(logStartLine),
+                    line
+            };
+            checkAnnotation(in, out);
+        }
+    }
+
+
+    private static void checkAnnotation(String[] in, String[] out) {
+        assertEquals(in.length, out.length);
+
+        Annotator annotator = new Annotator(null);
+        for (int i = 0; i < in.length; i++) {
+            MarkupText text = new MarkupText(in[i]);
+            Annotator result = annotator.annotate(null, text);
+
+            assertEquals(annotator, result);
+            System.out.println(out[i]);
+            System.out.println(text.toString(true));
+            assertEquals(out[i], text.toString(true));
+        }
+    }
+
+    private static String wrapSectionStart(String line, int idNum) {
+        return String.format("<input type=\"checkbox\" id=\"hide-block-%d\" class=\"hide\"/>", idNum) +
+                String.format("<label for=\"hide-block-%d\">", idNum) +
+                "<span class=\"sectionLbl\">" +
+                line +
+                "</label>" +
+                "</span>" +
+                "<div>";
+    }
+
+    private static String wrapSectionContent(String line) {
+        return "<span style=\"display: inline-block;  width: 4ch;\"></span>" + line;
+    }
+
+    private static String wrapSectionEnd(String line) {
+        return "<span style=\"display: inline-block;  width: 4ch;\"></span>" +
+                line +
+                "</div>" +
+                "<span class=\"nl\"></span>";
+    }
+
+    private static String replaceWithHtmlEntities(String line) {
+        return line.replaceAll(">", "&gt;").replaceAll("<", "&lt;");
+    }
+}
