618b0ddb295ef44b8254d038eadc3cda5039c1be
miltolstoy <miltolstoy@gmail.com>
2020-01-23 07:59:35 +0200

    set PYTHONUNBUFFERED to subprocess env only

--- universum_log_collapser/e2e/universum_live_log_to_html.py
+++ universum_log_collapser/e2e/universum_live_log_to_html.py
@@ -6,10 +6,11 @@ import re
 
 
 def run_universum():
-    os.environ["PYTHONUNBUFFERED"] = "1"
+    env = os.environ.copy()
+    env["PYTHONUNBUFFERED"] = "1"
     cmd = """universum --clean-build --vcs-type=none --server-type=local --out-type=term 
         --launcher-output=console --file-source-dir=. --launcher-config-path=./universum_config/configs.py"""
-    stdout = launch_process(*cmd.split())
+    stdout = launch_process(*cmd.split(), env=env)
     return re.sub("\\[.+?m", "", stdout)
 
 
@@ -21,8 +22,8 @@ def run_plugin_cli(log):
     return launch_process(*java_cmd.split(), log)
 
 
-def launch_process(*args):
-    popen = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+def launch_process(*args, env=None):
+    popen = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env)
     assert popen.wait() == 0, popen.stderr.read().decode()
     stdout = popen.stdout.read().decode()
     assert stdout
