191d629573e430435c65f5fdec63766d8178478a
k-dovgan <k.dovgan@samsung.com>
2020-05-14 16:29:35 +0300

    fix(config): run pylint out of venv

--- tests/configs.py
+++ tests/configs.py
@@ -9,9 +9,6 @@ def run_virtual(cmd):
     return ["env", "-i", "PATH=" + os.getenv("PATH"), "bash", "-c", "source {}/bin/activate; {}".format(env_name, cmd)]
 
 
-pylint_cmd = "universum_pylint --python-version 3 --rcfile pylintrc " + \
-             "--files *.py universum/ tests/ analyzers/ --result-file '${CODE_REPORT_FILE}'"
-
 configs = Variations([dict(name="Update Docker images", command=["make", "images"]),
                       dict(name="Create virtual environment",
                            command=["python3.7", "-m", "venv", env_name]),
@@ -23,8 +20,11 @@ configs = Variations([dict(name="Update Docker images", command=["make", "images
                            command=run_virtual("python3.7 -m pip --default-timeout=1200 install .[test] -U")),
                       dict(name="Make tests", artifacts="htmlcov",
                            command=run_virtual("export LANG=en_US.UTF-8; make test")),
+
                       dict(name="Run static pylint", code_report=True,
-                           command=run_virtual("python3.7 -m pip uninstall -y universum; " + pylint_cmd)),
+                           command=["universum_pylint", "--python-version=3.7", "--rcfile=pylintrc ",
+                                    "--files", "*.py", "universum/", "tests/", "analyzers/",
+                                    "--result-file='${CODE_REPORT_FILE}'"]),
 
                       dict(name="Run Jenkins plugin Java tests",
                            artifacts="universum_log_collapser/universum_log_collapser/target/surefire-reports/*.xml",
