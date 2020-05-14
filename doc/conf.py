02b0bf7f0c1ba6537b5ed510d7dde238598bf4ec
k-dovgan <k.dovgan@samsung.com>
2020-05-14 13:54:41 +0300

    fix(docs): also change directory name in documentation

--- doc/conf.py
+++ doc/conf.py
@@ -25,9 +25,7 @@
 # needs_sphinx = '1.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-from __future__ import absolute_import
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.coverage',
               'sphinx.ext.doctest',
