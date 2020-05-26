b0c6c92120255e224eef2aaef4e1776c9847b54e
k-dovgan <k.dovgan@samsung.com>
2020-05-26 15:53:23 +0300

    feat(github): add GitHub App webhook handler

---
+++ universum/github_hadler.py
@@ -0,0 +1,22 @@
+from .modules.automation_server.jenkins_server import JenkinsServerForTrigger
+
+from .lib.utils import make_block
+
+class GithubHandler(JenkinsServerForTrigger):
+
+    @staticmethod
+    def define_arguments(argument_parser):
+        argument_parser.add_argument('--event', '-e', dest='header',
+                                     help='Currently parsed from "x-github-event" header')
+        argument_parser.add_argument('--payload', '-op', dest='json',
+                                     help='Payload JSON object')
+
+    def __init__(self, *args, **kwargs):
+        super(GithubHandler, self).__init__(*args, **kwargs)
+
+    @make_block("Enumerating changes")
+    def execute(self):
+        pass
+
+    def finalize(self):
+        pass
