34b8cd62442bbf3a3de5bc53c6688265872e76e3
k-dovgan <k.dovgan@samsung.com>
2020-06-04 17:07:53 +0300

    refacto(github_handler): move parsing JSON to 'execute' for better exception handling

--- universum/github_handler.py
+++ universum/github_handler.py
@@ -12,9 +12,6 @@ from .lib.utils import make_block
 @needs_structure
 @needs_output
 class GithubHandler(JenkinsServerForTrigger, GithubToken):
-    """
-    Excepts payload from stdin!
-    """
 
     @staticmethod
     def define_arguments(argument_parser):
@@ -25,33 +22,36 @@ class GithubHandler(JenkinsServerForTrigger, GithubToken):
 
     def __init__(self, *args, **kwargs):
         super(GithubHandler, self).__init__(*args, **kwargs)
-        if self.settings.payload == '-':
-            self.payload = json.loads(sys.stdin.read())
-        else:
-            self.payload = json.loads(self.settings.payload)
+        # TODO: add checks for params; add tests
 
     @make_block("Analysing trigger payload")
     def execute(self):
-        if self.settings.event == "check_suite" and (self.payload["action"] in ["requested", "rerequested"]):
-            url = self.payload["repository"]["url"] + "/check-runs"
-            data = {"name": "CI tests", "head_sha": self.payload["check_suite"]["head_sha"]}
-            headers = {'Authorization': f"token {self.get_token(self.payload['installation']['id'])}",
+        # TODO: refactor to real curl-style variable with '@' adn '-' support
+        if self.settings.payload == '-':
+            payload = json.loads(sys.stdin.read())
+        else:
+            payload = json.loads(self.settings.payload)
+
+        if self.settings.event == "check_suite" and (payload["action"] in ["requested", "rerequested"]):
+            url = payload["repository"]["url"] + "/check-runs"
+            data = {"name": "CI tests", "head_sha": payload["check_suite"]["head_sha"]}
+            headers = {'Authorization': f"token {self.get_token(payload['installation']['id'])}",
                        'Accept': 'application/vnd.github.antiope-preview+json'}
             response = requests.post(url=url, json=data, headers=headers)
             response.raise_for_status()
 
         elif self.settings.event == "check_run" and \
-                (self.payload["action"] in ["requested", "rerequested", "created"]) and \
-                (str(self.payload["check_run"]["app"]["id"]) == str(self.settings.integration_id)):
+                (payload["action"] in ["requested", "rerequested", "created"]) and \
+                (str(payload["check_run"]["app"]["id"]) == str(self.settings.integration_id)):
             self.trigger_build({
-                "GIT_REFSPEC": self.payload["check_run"]["check_suite"]["head_branch"],
-                "GIT_CHECKOUT_ID": self.payload["check_run"]["head_sha"],
-                "GITHUB_CHECK_ID": self.payload["check_run"]["id"],
-                "GIT_REPO": self.payload["repository"]["clone_url"],
+                "GIT_REFSPEC": payload["check_run"]["check_suite"]["head_branch"],
+                "GIT_CHECKOUT_ID": payload["check_run"]["head_sha"],
+                "GITHUB_CHECK_ID": payload["check_run"]["id"],
+                "GIT_REPO": payload["repository"]["clone_url"],
                 "GITHUB_APP_ID": self.settings.integration_id,
-                "GITHUB_INSTALLATION_ID": self.payload['installation']['id'],
+                "GITHUB_INSTALLATION_ID": payload['installation']['id'],
                 "GITHUB_PRIVATE_KEY": self.settings.key_path,
-                "GITHUB_TOKEN": self.get_token(self.payload['installation']['id'])  # remove after tests
+                "GITHUB_TOKEN": self.get_token(payload['installation']['id'])  # remove after tests
             })
 
         else:
