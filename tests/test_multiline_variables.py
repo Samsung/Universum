16690f2ecb2e9018d684ed4364b80c1328d958bc
k-dovgan <k.dovgan@samsung.com>
2020-06-18 16:35:27 +0300

    refactor(test): remove subprocess use

--- tests/test_multiline_variables.py
+++ tests/test_multiline_variables.py
@@ -1,34 +1,17 @@
-import subprocess
-
-
-script = """
-from universum.lib.module_arguments import ModuleArgumentParser
 from universum.lib.utils import read_multiline_option
 
-parser = ModuleArgumentParser()
-# parser.add_argument('--argument', '-a', metavar='ARGUMENT')
-parser.add_argument('--argument', '-a')
 
-namespace = parser.parse_args()
-print(read_multiline_option(namespace.argument))
-"""
-
-
-def test_success_multiline_variable(tmp_path):
+def test_success_multiline_variable(tmp_path, monkeypatch):
     text = "This is text\nwith some line breaks\n"
 
     # Direct input
-    script_path = tmp_path / "script.py"
-    script_path.write_text(script)
-    result = subprocess.run(["python3.7", script_path, "-a", text], capture_output=True, text=True)
-    assert result.stdout[:-1] == text
+    assert read_multiline_option(text) == text
 
     # File
     var_path = tmp_path / "variable.txt"
     var_path.write_text(text)
-    result = subprocess.run(["python3.7", script_path, "-a", '@' + str(var_path)], capture_output=True, text=True)
-    assert result.stdout[:-1] == text
+    assert read_multiline_option(f"@{str(var_path)}") == text
 
     # Stdin
-    result = subprocess.run(["python3.7", script_path, "-a", '-'], capture_output=True, text=True, input=text)
-    assert result.stdout[:-1] == text
+    monkeypatch.setattr('sys.stdin.read', lambda: text)
+    assert read_multiline_option('-') == text
