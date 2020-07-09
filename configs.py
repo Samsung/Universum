7c5b4500d60dcd0091c576a58eab24bbb284c4cf
k-dovgan <k.dovgan@samsung.com>
2020-07-09 19:07:01 +0300

    fix(configs): remove pip progress bars (requires pip>=10)

--- configs.py
+++ configs.py
@@ -10,22 +10,26 @@ def run_virtual(cmd):
     return ["env", "-i", "PATH=" + os.getenv("PATH"), "bash", "-c", "source {}/bin/activate && {}".format(env_name, cmd)]
 
 
+def pip_install(module_name):
+    return "python3.7 -m pip --default-timeout=1200 install --progress-bar off -U " + module_name
+
+
 configs = Variations([dict(name="Update Docker images", command=["make", "images"]),
-                      dict(name="Update Pylint", command=["python3.7", "-m", "pip", "install", "-U", "--user", "pylint"]),
+                      dict(name="Update Pylint", command=["python3.7", "-m", "pip", "install", "-U", "--user",
+                                                          "--progress-bar", "off", "pylint"]),
                       dict(name="Create virtual environment",
                            command=["python3.7", "-m", "venv", env_name]),
-                      dict(name="Install development",
-                           command=run_virtual("python3.7 -m pip --default-timeout=1200 install .[development] -U")),
+                      dict(name="Install development", command=run_virtual(pip_install(".[development]"))),
                       dict(name="Make", artifacts="doc/_build", command=run_virtual("make")),
 
                       dict(name="Install tests", artifacts="junit_results.xml",
-                           command=run_virtual("python3.7 -m pip --default-timeout=1200 install .[test] -U")),
+                           command=run_virtual(pip_install(".[test]"))),
                       dict(name="Make tests", artifacts="htmlcov",
                            command=run_virtual("export LANG=en_US.UTF-8; make test")),
 
                       dict(name="Run static pylint", code_report=True,
-                           command=["python3.7", "-m", "universum.analyzers.pylint",
-                                    "--python-version=3.7", "--rcfile=pylintrc", "--result-file=${CODE_REPORT_FILE}",
+                           command=["python3.7", "-m", "universum.analyzers.pylint", "--python-version=3.7",
+                                    "--rcfile=pylintrc", "--result-file=${CODE_REPORT_FILE}",
                                     "--files", "*.py", "universum/", "tests/"]),
 
                       dict(name="Run Jenkins plugin Java tests",
