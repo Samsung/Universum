2182e1a31514fe9e8c95288ee99a4398c1ebda18
i-keliukh <46344924+i-keliukh@users.noreply.github.com>
2020-07-31 18:43:25 +0300

    fix(vcs): raw exceptions are shown during finalize

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -1,5 +1,8 @@
 import pytest
 
+from universum import __main__
+from . import utils
+
 
 @pytest.fixture(name='print_text_on_teardown')
 def fixture_print_text_on_teardown():
@@ -9,3 +12,25 @@ def fixture_print_text_on_teardown():
 
 def test_teardown_fixture_output_verification(print_text_on_teardown):
     pass
+
+
+def test_clean_sources_exceptions(tmpdir):
+    env = utils.TestEnvironment(tmpdir, "main")
+    env.settings.Vcs.type = "none"
+    env.settings.LocalMainVcs.source_dir = str(tmpdir / 'nonexisting_dir')
+    env.settings.Main.no_diff = True  # TODO: remove when issue #477 is fixed
+
+    # Check failure with non-existing temp dir
+    __main__.run(env.settings)
+    # the log output is automatically checked by the 'detect_fails' fixture
+
+    # Check failure with temp dir deleted by the launched project
+    env.settings.LocalMainVcs.source_dir = str(tmpdir)
+    env.configs_file.write("""
+from universum.configuration_support import Variations
+
+configs = Variations([dict(name="Test configuration", command=["bash", "-c", "rm -rf {}"])])
+""".format(env.settings.ProjectDirectory.project_root))
+
+    __main__.run(env.settings)
+    # the log output is automatically checked by the 'detect_fails' fixture
