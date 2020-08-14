e39eac6043505375b86d50f8e74fd90cb167d173
k-dovgan <k.dovgan@samsung.com>
2020-08-14 13:02:22 +0300

    fix(test): add back the 'force_installed' mode for run()

--- tests/test_deployment.py
+++ tests/test_deployment.py
@@ -1,8 +1,3 @@
-import os
-
-from .utils import randomize_name
-
-
 config = """
 from universum.configuration_support import Variations
 
@@ -11,66 +6,45 @@ configs = Variations([dict(name="Test configuration", command=["ls", "-la"])])
 
 
 def test_minimal_install(clean_docker_main):
-    # Create tmpdir in container to make sure local universum sources are not used instead of installed
-    tmpdir = os.path.join(clean_docker_main.working_dir, randomize_name('tmpdir'))
-    clean_docker_main.environment.assert_successful_execution(f"mkdir '{tmpdir}'")
-
     # Run without parameters
     log = clean_docker_main.environment.assert_unsuccessful_execution("python3.7 -m universum")
     assert "No module named universum" not in log
 
     # Run locally
-    log = clean_docker_main.run(config, workdir=tmpdir)
+    log = clean_docker_main.run(config, force_installed=True)
     assert clean_docker_main.local.repo_file.basename in log
 
     # Run from Git
     clean_docker_main.clean_artifacts()
-    log = clean_docker_main.run(config, vcs_type="git", workdir=tmpdir)
+    log = clean_docker_main.run(config, vcs_type="git", force_installed=True)
     assert clean_docker_main.git.repo_file.basename in log
 
     # Run from P4
     clean_docker_main.clean_artifacts()
-    log = clean_docker_main.run(config, vcs_type="p4", workdir=tmpdir)
+    log = clean_docker_main.run(config, vcs_type="p4", force_installed=True)
     assert clean_docker_main.perforce.repo_file.basename in log
-
-    # Clean
-    clean_docker_main.environment.assert_successful_execution(f"rm -rf '{tmpdir}'")
 
 
 def test_minimal_install_with_git_only(clean_docker_main_no_p4, capsys):
-    # Create tmpdir in container to make sure local universum sources are not used instead of installed
-    tmpdir = os.path.join(clean_docker_main_no_p4.working_dir, randomize_name('tmpdir'))
-    clean_docker_main_no_p4.environment.assert_successful_execution(f"mkdir '{tmpdir}'")
-
     # Run from P4
-    clean_docker_main_no_p4.run(config, vcs_type="p4", workdir=tmpdir, expected_to_fail=True)
+    clean_docker_main_no_p4.run(config, vcs_type="p4", force_installed=True, expected_to_fail=True)
     assert "Please refer to `Prerequisites` chapter of project documentation" in capsys.readouterr().out
 
     # Run from git
     clean_docker_main_no_p4.clean_artifacts()
-    log = clean_docker_main_no_p4.run(config, vcs_type="git", workdir=tmpdir)
+    log = clean_docker_main_no_p4.run(config, vcs_type="git", force_installed=True)
     assert clean_docker_main_no_p4.git.repo_file.basename in log
-
-    # Clean
-    clean_docker_main_no_p4.environment.assert_successful_execution(f"rm -rf '{tmpdir}'")
 
 
 def test_minimal_install_plain_ubuntu(clean_docker_main_no_vcs, capsys):
-    # Create tmpdir in container to make sure local universum sources are not used instead of installed
-    tmpdir = os.path.join(clean_docker_main_no_vcs.working_dir, randomize_name('tmpdir'))
-    clean_docker_main_no_vcs.environment.assert_successful_execution(f"mkdir '{tmpdir}'")
-
     # Run from P4
-    clean_docker_main_no_vcs.run(config, vcs_type="p4", workdir=tmpdir, expected_to_fail=True)
+    clean_docker_main_no_vcs.run(config, vcs_type="p4", force_installed=True, expected_to_fail=True)
     assert "Please refer to `Prerequisites` chapter of project documentation" in capsys.readouterr().out
 
     # Run from Git
-    clean_docker_main_no_vcs.run(config, vcs_type="git", workdir=tmpdir, expected_to_fail=True)
+    clean_docker_main_no_vcs.run(config, vcs_type="git", force_installed=True, expected_to_fail=True)
     assert "Please refer to `Prerequisites` chapter of project documentation" in capsys.readouterr().out
 
     # Run locally
-    log = clean_docker_main_no_vcs.run(config, workdir=tmpdir)
+    log = clean_docker_main_no_vcs.run(config, force_installed=True)
     assert clean_docker_main_no_vcs.local.repo_file.basename in log
-
-    # Clean
-    clean_docker_main_no_vcs.environment.assert_successful_execution(f"rm -rf '{tmpdir}'")
