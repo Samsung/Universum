13590a14404234d596a82338f5c3edeab35d7cc2
Ivan Keliukh <i.keliukh@samsung.com>
2020-08-20 11:03:01 +0300

    fix review comment

--- universum/lib/gravity.py
+++ universum/lib/gravity.py
@@ -96,7 +96,7 @@ class Dependency(Generic[DependencyType]):
     # The source and the target of the dependency are different modules,
     # so we need to use different type variables to annotate them.
     # instance parameter of the __get__ method is source module.
-    def __get__(self, instance: ComponentType, owner: Any) -> Callable[..., DependencyType]:
+    def __get__(self, instance: Module, owner: Any) -> Callable[..., DependencyType]:
         def constructor_function(*args, **kwargs) -> DependencyType:
             return construct_component(self.cls, instance.main_settings, *args, **kwargs)
         return constructor_function
