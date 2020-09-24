90c1c5349c7ee4c9766cd6e56010175837790ca6
Kateryna Dovgan <46348880+k-dovgan@users.noreply.github.com>
2020-09-24 16:38:08 +0300

    feat(launcher): add default config name

--- universum/config_creator.py
+++ universum/config_creator.py
@@ -27,8 +27,7 @@ configs = Variations([dict(name='Show directory contents', command=['ls', '-la']
 if __name__ == '__main__':
     print(configs.dump())
 """)
-        self.out.log("To run with Universum, execute the following command:\n"
-                     "$ python3.7 -m universum run --launcher-config-path={}".format(config_name))
+        self.out.log("To run with Universum, execute the following command:\n$ python3.7 -m universum run")
 
     def finalize(self) -> None:
         pass
--- universum/modules/launcher.py
+++ universum/modules/launcher.py
@@ -307,8 +307,8 @@ class Launcher(ProjectDirectory, HasOutput, HasStructure, HasErrorState):
                                                      "External command launching and reporting parameters")
 
         parser.add_argument("--config", "-cfg", dest="config_path", metavar="CONFIG_PATH",
-                            help="Path to project config file (example: -cfg=my/prject/my_conf.py). "
-                                 "Mandatory parameter")
+                            help="Path to project configuration file (example: -cfg=my/prject/my_conf.py). "
+                                 "Default is ``.universum.py``")
 
         parser.add_argument("--filter", "-f", dest="step_filter", action='append', metavar="STEP_FILTER",
                             help="Filter steps to execute. A single filter or a set of filters separated by ':'. "
@@ -333,18 +333,9 @@ class Launcher(ProjectDirectory, HasOutput, HasStructure, HasErrorState):
             else:
                 self.output = "console"
 
-        if not getattr(self.settings, "config_path", None):
-            self.error("""
-                The path to the config file is not specified.
-
-                The config defines all steps performed by Universum for your project. The status
-                of each step can be reported to the code review system. The config file for a
-                project can be created and debugged locally. There are examples of config files
-                in the documentation.
-
-                Please specify the path to project config file by using '--launcher-config-path'
-                ('-lcp') command-line option or CONFIG_PATH environment variable
-                """)
+        self.config_path = self.settings.config_path
+        if not self.config_path:
+            self.config_path = ".universum.py"
 
         self.artifacts = self.artifacts_factory()
         self.api_support = self.api_support_factory()
@@ -355,7 +346,7 @@ class Launcher(ProjectDirectory, HasOutput, HasStructure, HasErrorState):
 
     @make_block("Processing project configs")
     def process_project_configs(self):
-        config_path = utils.parse_path(self.settings.config_path, self.settings.project_root)
+        config_path = utils.parse_path(self.config_path, self.settings.project_root)
         configuration_support.set_project_root(self.settings.project_root)
         config_globals = {}
         sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
@@ -378,21 +369,21 @@ class Launcher(ProjectDirectory, HasOutput, HasStructure, HasErrorState):
         except IOError as e:
             text = f"""{e}\n
                 Possible reasons of this error:\n
-                * There is no file named 'configs.py' in project repository\n
-                * Config path, passed to the script ('{self.settings.config_path}'), does not lead to
-                 actual 'configs.py' location\n
+                * There is no Universum configuration file in project repository\n
+                * Config path, passed to the script ('{self.config_path}'),
+                  does not lead to actual Universum configuration file location\n
                 * Some problems occurred while downloading or copying the repository
                 """
             raise CriticalCiException(cleandoc(text)) from e
         except KeyError as e:
             text = "KeyError: " + str(e) + '\n'
-            text += "Possible reason of this error: variable 'configs' is not defined in 'configs.py'"
+            text += "Possible reason of this error: variable 'configs' is not defined in Universum configuration file"
             raise CriticalCiException(text) from e
         except Exception as e:
             ex_traceback = sys.exc_info()[2]
-            text = "Exception while processing 'configs.py'\n" + utils.format_traceback(e, ex_traceback) + \
-                   "\nPlease copy 'configs.py' script to the CI scripts folder and run it " + \
-                   "to make sure no exceptions occur in that case."
+            text = "Exception while processing Universum configuration file:\n" + \
+                   utils.format_traceback(e, ex_traceback) + \
+                   "\nTry to execute ``confgs.dump()`` to make sure no exceptions occur in that case."
             raise CriticalCiException(text) from e
         return self.project_configs
 
