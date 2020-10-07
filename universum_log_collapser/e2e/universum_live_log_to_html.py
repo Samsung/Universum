bb8e33b9d5ed6f546c901f9fbae97bc1c0d66c5e
Oleksandr Andrieiev <o.andrieiev@samsung.com>
2020-10-07 09:07:11 -0400

    remove debug logs

--- universum_log_collapser/e2e/universum_live_log_to_html.py
+++ universum_log_collapser/e2e/universum_live_log_to_html.py
@@ -52,6 +52,4 @@ def write_to_html(log):
 if __name__ == "__main__":
     raw_log = run_universum()
     wrapped_log = run_plugin_cli(raw_log)
-    print(raw_log)
     write_to_html(wrapped_log)
-    print(wrapped_log)
