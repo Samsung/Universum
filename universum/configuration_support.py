a7c994feca4b864752406120f3a89fe7b5ad878a
Oleksandr Andrieiev <o.andrieiev@samsung.com>
2020-10-12 06:49:01 -0400

    more doctest

--- universum/configuration_support.py
+++ universum/configuration_support.py
@@ -39,6 +39,8 @@ class ProjectConfiguration:
         All params supplied via named attributed shall be  type-checked for safety. Clients may provide custom
         parameters in kwargs - these parameters will be stored internally in '_extras' dict and can be retrieved by
         indexing.
+        >>> ProjectConfiguration(foo='bar')
+        {'foo': 'bar'}
         >>> cfg = ProjectConfiguration(name='foo', command=['1', '2', '3'], _extras={'_extras': 'test', 'bool': True})
         >>> cfg
         {'name': 'foo', 'command': ['1', '2', '3'], '_extras': {'_extras': 'test', 'bool': True}}
