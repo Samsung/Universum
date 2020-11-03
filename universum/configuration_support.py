0d8d2a25cabc5ff8606857ca65b9956e26b63cd0
Oleksandr Andrieiev <o.andrieiev@samsung.com>
2020-11-03 10:12:25 -0500

    fix

--- universum/configuration_support.py
+++ universum/configuration_support.py
@@ -19,13 +19,14 @@ class Step:
     """
     Step is a collection of configurable project-specific data entries needed for Universum functionality. Individual
     build steps are collected in a :class:`Configuration` object, which supports arithmetic operations for
-    combination and chaining. All params supplied via named attributes may be type-checked for safety. Clients may
-    provide custom parameters in kwargs - these parameters will be stored internally in `dict` and can be retrieved
-    by indexing.
+    combination and chaining.
 
-    .. Step keys
+    Step keys
+    ---------
 
-    The following list enumerates all the keys supported by Universum.
+    The following list enumerates all the keys supported by Universum. Parameters supplied via named attributes during
+    construction are annotated and can be type-checked for safety. Clients may also provide custom parameters in
+    kwargs - these parameters will be stored internally in `dict` and can be retrieved by indexing.
 
     name
         The human-readable name of a build step. The name is used as the title of the build log block
