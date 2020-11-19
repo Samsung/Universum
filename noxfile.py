c3412567e6fe0822c84981405b0bb620b96f679f
k-dovgan <k.dovgan@samsung.com>
2020-11-19 18:46:41 +0200

    fix(nox): update noxfile up to new make targets

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,8 @@
-import nox
+import nox  # pylint: disable = import-error
 
 
 @nox.session(python=["3.6", "3.7", "3.8"])
 def test(session):
-    session.run("make", "rebuild_python" + session.python)
+    session.run("make", "rebuild")
     session.install("universum[test]")
     session.run("make", "test")
