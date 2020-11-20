b37d068330467efc8572f52d951d585488e68f30
k-dovgan <k.dovgan@samsung.com>
2020-11-20 16:19:43 +0200

    update(nox): remove unnecessary lines from logs

--- noxfile.py
+++ noxfile.py
@@ -3,6 +3,6 @@ import nox
 
 @nox.session(python=["3.6", "3.7", "3.8"])
 def test(session):
-    session.run("make", "rebuild")
+    session.run("make", "rebuild", silent=True, external=True)
     session.install("universum[test]")
-    session.run("make", "test")
+    session.run("make", "test", external=True)
