641f3c24404a93536d5b4720dc44fbd9d6ec0951
k-dovgan <k.dovgan@samsung.com>
2020-11-26 17:07:43 +0200

    fix(test): apply changes from review

--- tests/test_create_config.py
+++ tests/test_create_config.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 import os
 import subprocess
 
@@ -6,16 +5,11 @@ from .utils import python
 
 
 def test_create_config(tmpdir):
-    test_environment = dict(os.environ, PYTHONPATH=os.getcwd())
-    result = subprocess.run([python(), "-m", "universum", "init"],
-                            capture_output=True, check=True, cwd=tmpdir, env=test_environment)
+    launch_parameters = dict(capture_output=True, check=True, cwd=tmpdir, env=dict(os.environ, PYTHONPATH=os.getcwd()))
+    result = subprocess.run([python(), "-m", "universum", "init"], **launch_parameters)
     new_command = ''
     for line in result.stdout.splitlines():
         if line.startswith(b'$ '):           # result.stdout is a byte string
             new_command = line.lstrip(b'$ ')
-    new_result = subprocess.run(new_command.split(), capture_output=True, check=True, cwd=tmpdir, env=test_environment)
-    Path(tmpdir, ".universum.py").unlink()
-    artifacts = Path(tmpdir, "artifacts")
-    artifacts.joinpath("CONFIGS_DUMP.txt").unlink()
-    artifacts.rmdir()
+    new_result = subprocess.run(new_command.split(), **launch_parameters)
     assert 'Hello world' in str(new_result.stdout)
