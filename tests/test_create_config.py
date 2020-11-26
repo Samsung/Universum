eb5910ce3f3d3de8e1b312ae57b23222f050ba84
k-dovgan <k.dovgan@samsung.com>
2020-11-26 16:45:09 +0200

    fix(test): add current directory to PYTHONPATH
    Note that at this point the PYTHONPATH should be empty, and all
    additinal values will be added by Python later

--- tests/test_create_config.py
+++ tests/test_create_config.py
@@ -1,16 +1,20 @@
 from pathlib import Path
+import os
 import subprocess
 
 from .utils import python
 
 
 def test_create_config(tmpdir):
-    result = subprocess.run([python(), "-m", "universum", "init"], capture_output=True, check=True, cwd=tmpdir)
+    my_env = os.environ.copy()
+    my_env["PYTHONPATH"] = os.getcwd()
+    result = subprocess.run([python(), "-m", "universum", "init"],
+                            capture_output=True, check=True, cwd=tmpdir, env=my_env)
     new_command = ''
     for line in result.stdout.splitlines():
         if line.startswith(b'$ '):           # result.stdout is a byte string
             new_command = line.lstrip(b'$ ')
-    new_result = subprocess.run(new_command.split(), capture_output=True, check=True, cwd=tmpdir)
+    new_result = subprocess.run(new_command.split(), capture_output=True, check=True, cwd=tmpdir, env=my_env)
     Path(tmpdir, ".universum.py").unlink()
     artifacts = Path(tmpdir, "artifacts")
     artifacts.joinpath("CONFIGS_DUMP.txt").unlink()
