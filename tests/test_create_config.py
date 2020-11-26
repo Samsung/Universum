b207da0810b63c1461351151d863c01d0fe580ac
k-dovgan <k.dovgan@samsung.com>
2020-11-26 15:04:10 +0200

    fix(test): create-config test interfering with actual config

--- tests/test_create_config.py
+++ tests/test_create_config.py
@@ -4,15 +4,15 @@ import subprocess
 from .utils import python
 
 
-def test_create_config():
-    result = subprocess.run([python(), "-m", "universum", "init"], capture_output=True, check=True)
+def test_create_config(tmpdir):
+    result = subprocess.run([python(), "-m", "universum", "init"], capture_output=True, check=True, cwd=tmpdir)
     new_command = ''
     for line in result.stdout.splitlines():
         if line.startswith(b'$ '):           # result.stdout is a byte string
             new_command = line.lstrip(b'$ ')
-    new_result = subprocess.run(new_command.split(), capture_output=True, check=True)
-    Path(".universum.py").unlink()
-    artifacts = Path("artifacts")
+    new_result = subprocess.run(new_command.split(), capture_output=True, check=True, cwd=tmpdir)
+    Path(tmpdir, ".universum.py").unlink()
+    artifacts = Path(tmpdir, "artifacts")
     artifacts.joinpath("CONFIGS_DUMP.txt").unlink()
     artifacts.rmdir()
     assert 'Hello world' in str(new_result.stdout)
