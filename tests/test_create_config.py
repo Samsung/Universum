806ff9d8d90c7401c880717f10cfa52c1cab382c
Kateryna Dovgan <46348880+k-dovgan@users.noreply.github.com>
2020-11-26 20:03:22 +0200

    fix(test): create-config test interfering with actual config

--- tests/test_create_config.py
+++ tests/test_create_config.py
@@ -1,18 +1,15 @@
-from pathlib import Path
+import os
 import subprocess
 
 from .utils import python
 
 
-def test_create_config():
-    result = subprocess.run([python(), "-m", "universum", "init"], capture_output=True, check=True)
+def test_create_config(tmpdir):
+    launch_parameters = dict(capture_output=True, cwd=tmpdir, env=dict(os.environ, PYTHONPATH=os.getcwd()))
+    result = subprocess.run([python(), "-m", "universum", "init"], check=True, **launch_parameters)
     new_command = ''
     for line in result.stdout.splitlines():
         if line.startswith(b'$ '):           # result.stdout is a byte string
             new_command = line.lstrip(b'$ ')
-    new_result = subprocess.run(new_command.split(), capture_output=True, check=True)
-    Path(".universum.py").unlink()
-    artifacts = Path("artifacts")
-    artifacts.joinpath("CONFIGS_DUMP.txt").unlink()
-    artifacts.rmdir()
+    new_result = subprocess.run(new_command.split(), check=True, **launch_parameters)
     assert 'Hello world' in str(new_result.stdout)
