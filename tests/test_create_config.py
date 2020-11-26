159614758ec49e77a77d12b45a41d88580e0468d
k-dovgan <k.dovgan@samsung.com>
2020-11-26 16:58:21 +0200

    fix(test): clean up environment changes

--- tests/test_create_config.py
+++ tests/test_create_config.py
@@ -6,15 +6,14 @@ from .utils import python
 
 
 def test_create_config(tmpdir):
-    my_env = os.environ.copy()
-    my_env["PYTHONPATH"] = os.getcwd()
+    test_environment = dict(os.environ, PYTHONPATH=os.getcwd())
     result = subprocess.run([python(), "-m", "universum", "init"],
-                            capture_output=True, check=True, cwd=tmpdir, env=my_env)
+                            capture_output=True, check=True, cwd=tmpdir, env=test_environment)
     new_command = ''
     for line in result.stdout.splitlines():
         if line.startswith(b'$ '):           # result.stdout is a byte string
             new_command = line.lstrip(b'$ ')
-    new_result = subprocess.run(new_command.split(), capture_output=True, check=True, cwd=tmpdir, env=my_env)
+    new_result = subprocess.run(new_command.split(), capture_output=True, check=True, cwd=tmpdir, env=test_environment)
     Path(tmpdir, ".universum.py").unlink()
     artifacts = Path(tmpdir, "artifacts")
     artifacts.joinpath("CONFIGS_DUMP.txt").unlink()
