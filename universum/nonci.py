783c1498c69c38ec4989af2a2d037a04c79ff921
Kateryna Dovgan <46348880+k-dovgan@users.noreply.github.com>
2020-12-04 18:15:05 +0200

    fix(nonci): remove file postprocessing

--- universum/nonci.py
+++ universum/nonci.py
@@ -20,13 +20,10 @@ class Nonci(Launcher):
         self.artifacts.clean_artifacts_silently()
 
         project_configs = self.process_project_configs()
-        afterall_configs = self.code_report_collector.prepare_environment(project_configs)
+        self.code_report_collector.prepare_environment(project_configs)
         self.artifacts.set_and_clean_artifacts(project_configs, ignore_existing_artifacts=True)
 
         self.launch_project()
-        if afterall_configs:
-            self.code_report_collector.report_code_report_results()
-
         self.reporter.report_initialized = True
         self.reporter.report_build_result()
         self.artifacts.collect_artifacts()
