e73b3ce2791cf8dada40fe24f484eb5f9fe85187
Kateryna Dovgan <46348880+k-dovgan@users.noreply.github.com>
2020-12-07 14:10:31 +0200

    fix(nonci): 'universum run' should still process analyzers correctly

--- universum/nonci.py
+++ universum/nonci.py
@@ -16,11 +16,11 @@ class Nonci(Launcher):
         super().__init__(*args, **kwargs)
 
     def execute(self):
-
         self.out.log("Cleaning artifacts...")
         self.artifacts.clean_artifacts_silently()
 
         project_configs = self.process_project_configs()
+        self.code_report_collector.prepare_environment(project_configs)
         self.artifacts.set_and_clean_artifacts(project_configs, ignore_existing_artifacts=True)
 
         self.launch_project()
