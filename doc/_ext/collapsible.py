02dfb217a6ecca8e92ebd43c92d152fcb99a7909
k-dovgan <k.dovgan@samsung.com>
2020-12-10 12:21:46 +0200

    wip(docs): randomize ids for collapsible sections

--- doc/_ext/collapsible.py
+++ doc/_ext/collapsible.py
@@ -1,3 +1,6 @@
+import random
+import string
+
 from docutils import nodes
 from docutils.parsers.rst import Directive
 from docutils.parsers.rst.directives import unchanged
@@ -9,9 +12,9 @@ class CollapsibleNode(nodes.General, nodes.Element):
 
 def visit_collapsible_node(self, node):
     self.body.append(f"""
-<input type="checkbox" id="collapsible" class="hide">
-<label for="collapsible">
-    <span class="sectionLbl"><b>{ node["header"] }</b></span>
+<input type="checkbox" id="{node["id"]}" class="hide">
+<label for="{node["id"]}">
+    <span class="sectionLbl"><b>{node["header"]}</b></span>
 </label>
 <div>
 """)
@@ -25,12 +28,17 @@ def depart_collapsible_node(self, node):
 """)
 
 
+def randomize_name(name):
+    return name + "-" + "".join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+
+
 class CollapsibleDirective(Directive):
     has_content = True
     option_spec = {'header': unchanged}
 
     def run(self):
         collapsible_node = CollapsibleNode('\n'.join(self.content),
+                                           id=randomize_name("collapsible"),
                                            header=self.options.get("header", "Click here to show/hide"))
         self.state.nested_parse(self.content, self.content_offset, collapsible_node)
 
