7f0f123e6bf4d7bd47eddb61494f898610d23cd6
o-andrieiev <40266373+o-andrieiev@users.noreply.github.com>
2021-05-05 17:07:21 +0300

    feat(analyzer): add mypy
    add mypy analyzer wrapper
    use wrapper internally in config
    refactor analyzer test cases for extensibility

--- tests/test_code_report.py
+++ tests/test_code_report.py
@@ -10,49 +10,70 @@ from . import utils
 from .utils import python, python_version
 
 
-@pytest.fixture(name='runner_with_pylint')
-def fixture_runner_with_pylint(docker_main):
+@pytest.fixture(name='runner_with_analyzers')
+def fixture_runner_with_analyzers(docker_main):
     docker_main.environment.install_python_module("pylint")
+    docker_main.environment.install_python_module("mypy")
     yield docker_main
 
 
-def get_config(args: List[str]):
-    args = [f", '{arg}'" for arg in args]
-    return inspect.cleandoc(f"""
-        from universum.configuration_support import Configuration
+class ConfigData:
+    def __init__(self):
+        self.text = "from universum.configuration_support import Configuration, Step\n"
+        self.text += "configs = Configuration()\n"
 
-        configs = Configuration([dict(name="Run static pylint", code_report=True,
-            command=['{python()}', '-m', 'universum.analyzers.pylint'{''.join(args)}])])
-    """)
+    def add_analyzer(self, analyzer: str, arguments: List[str]) -> 'ConfigData':
+        args = [f", '{arg}'" for arg in arguments]
+        cmd = f"['{python()}', '-m', 'universum.analyzers.{analyzer}'{''.join(args)}]"
+        self.text += f"configs += Configuration([Step(name='Run {analyzer}', code_report=True, command={cmd})])\n"
+        return self
+
+    def finalize(self) -> str:
+        return inspect.cleandoc(self.text)
 
 
-source_code = """
+source_code_python = """
 "Docstring."
 
-print("Hello world.")
+s: str = "world"
+print(f"Hello {s}.")
 """
 
+
 log_fail = r'Found [0-9]+ issues'
-log_success = r'Issues not found.'
+log_success = r'Issues not found'
 
 
-@pytest.mark.parametrize('args, tested_content, expected_log', [
-    [["--result-file", "${CODE_REPORT_FILE}"], source_code, log_success],
-    [["--result-file", "${CODE_REPORT_FILE}"], source_code + '\n', log_fail]
-
+@pytest.mark.parametrize('analyzers, extra_args, tested_content, expected_success', [
+    [['pylint', 'mypy'], ["--python-version", python_version()], source_code_python, True],
+    [['pylint'], ["--python-version", python_version()], source_code_python + '\n', False],
+    [['mypy'], ["--python-version", python_version()], source_code_python.replace(': str', ': int'), False],
+    [['pylint', 'mypy'], ["--python-version", python_version()], source_code_python.replace(': str', ': int') + '\n', False],
     # TODO: add test with rcfile
     # TODO: parametrize test for different versions of python
 ])
-def test_code_report(runner_with_pylint, args, tested_content, expected_log):
-    runner_with_pylint.local.root_directory.join("source_file.py").write(tested_content)
-    config = get_config(["--python-version", python_version(), "--files", "source_file.py"] + args)
+def test_code_report_log(runner_with_analyzers, analyzers, extra_args, tested_content, expected_success):
+    common_args = [
+        "--result-file", "${CODE_REPORT_FILE}",
+        "--files", "source_file",
+    ]
+    runner_with_analyzers.local.root_directory.join("source_file").write(tested_content)
+    config = ConfigData()
+    for analyzer in analyzers:
+        args = common_args + extra_args
+        config.add_analyzer(analyzer, args)
 
-    log = runner_with_pylint.run(config)
-    assert re.findall(expected_log, log)
+    log = runner_with_analyzers.run(config.finalize())
+    if expected_success:
+        assert re.findall(log_success, log)
+    else:
+        assert re.findall(log_fail, log)
+        for analyzer in analyzers:  # confirm that all analyzers fail independently
+            assert re.findall(fr'Run {analyzer} - [^\n]*Failed', log)
 
 
-def test_without_code_report_command(runner_with_pylint):
-    log = runner_with_pylint.run("""
+def test_without_code_report_command(runner_with_analyzers):
+    log = runner_with_analyzers.run("""
 from universum.configuration_support import Configuration
 
 configs = Configuration([dict(name="Run usual command", command=["ls", "-la"])])
@@ -61,25 +82,32 @@ configs = Configuration([dict(name="Run usual command", command=["ls", "-la"])])
     assert not pattern.findall(log)
 
 
-@pytest.mark.parametrize('args, expected_log', [
-    [["--python-version", python_version(), "--files", "source_file.py", "--result-file", "${CODE_REPORT_FILE}", '--rcfile'],
-     'rcfile: expected one argument'],
+@pytest.mark.parametrize('analyzer', ['pylint', 'mypy'])
+@pytest.mark.parametrize('common_arg_set, expected_log', [
     [["--python-version", python_version(), "--files", "source_file.py", "--result-file"],
      'result-file: expected one argument'],
     [["--python-version", python_version(), "--files", "--result-file", "${CODE_REPORT_FILE}"],
      "files: expected at least one argument"],
-
     [["--python-version", "--files", "source_file.py", "--result-file", "${CODE_REPORT_FILE}"],
      "python-version: expected one argument"],
     [["--python-version", python_version(), "--result-file", "${CODE_REPORT_FILE}"],
      "error: the following arguments are required: --files"],
 ])
-def test_pylint_analyzer_wrong_params(runner_with_pylint, args, expected_log):
-    source_file = runner_with_pylint.local.root_directory.join("source_file.py")
-    source_file.write(source_code)
+def test_pylint_analyzer_wrong_common_params(runner_with_analyzers, analyzer, common_arg_set, expected_log):
+    test_pylint_analyzer_wrong_specific_params(runner_with_analyzers, analyzer, common_arg_set, expected_log)
 
-    log = runner_with_pylint.run(get_config(args))
-    assert re.findall(r'Run static pylint - [^\n]*Failed', log)
+
+@pytest.mark.parametrize('analyzer, arg_set, expected_log', [
+    ['pylint', ["--python-version", python_version(), "--files", "source_file",
+                "--result-file", "${CODE_REPORT_FILE}", '--rcfile'],
+     "rcfile: expected one argument"],
+])
+def test_pylint_analyzer_wrong_specific_params(runner_with_analyzers, analyzer, arg_set, expected_log):
+    source_file = runner_with_analyzers.local.root_directory.join("source_file")
+    source_file.write(source_code_python)
+
+    log = runner_with_analyzers.run(ConfigData().add_analyzer(analyzer, arg_set).finalize())
+    assert re.findall(fr'Run {analyzer} - [^\n]*Failed', log)
     assert expected_log in log
 
 
@@ -88,14 +116,15 @@ def test_code_report_extended_arg_search(tmpdir, stdout_checker):
     env.settings.Vcs.type = "none"
     env.settings.LocalMainVcs.source_dir = str(tmpdir)
 
-    tmpdir.join("source_file.py").write(source_code + '\n')
+    source_file = tmpdir.join("source_file.py")
+    source_file.write(source_code_python + '\n')
 
     config = f"""
 from universum.configuration_support import Configuration
 
 configs = Configuration([dict(name="Run static pylint", code_report=True, artifacts="${{CODE_REPORT_FILE}}", command=[
     'bash', '-c', 'cd "{os.getcwd()}" && {python()} -m universum.analyzers.pylint --result-file="${{CODE_REPORT_FILE}}" \
-                   --python-version {python_version()} --files {str(tmpdir.join("source_file.py"))}'])])
+                   --python-version {python_version()} --files {str(source_file)}'])])
 """
 
     env.configs_file.write(config)
