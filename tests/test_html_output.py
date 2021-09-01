cfb833166d3ad9ee15eb8393db7af2389b9624aa
miltolstoy <miltolstoy@gmail.com>
2021-09-01 07:46:06 +0300

    feat(output): add sections collapsing in HTML output

--- tests/test_html_output.py
+++ tests/test_html_output.py
@@ -5,6 +5,7 @@ import pytest
 
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.webelement import FirefoxWebElement
 
 
 config = """
@@ -55,10 +56,101 @@ def check_html_log(artifact_dir, browser):
     assert os.path.exists(log_path)
 
     browser.get(f"file://{log_path}")
-    body_element = browser.find_element_by_tag_name("body")
-    body_elements = body_element.find_elements_by_xpath(".//*")
+    html_body = browser.find_element_by_tag_name("body")
+    body_elements = html_body.find_elements_by_xpath("./*")
     assert len(body_elements) == 1
+    assert body_elements[0].tag_name == "pre"
 
-    pre_element = body_elements[0]
-    assert pre_element.tag_name == "pre"
-    assert pre_element.text # will be changed to valid HTML in further commits
+    pre_element = TestElement.create(body_elements[0])
+    steps_section = pre_element.get_section_by_name("Executing build steps")
+    steps_body = steps_section.get_section_body()
+
+    check_step_collapsed(steps_section, steps_body)
+    steps_section.click()
+    check_step_not_collapsed(steps_section, steps_body)
+    check_sections_indentation(steps_section)
+    steps_section.click()
+    check_step_collapsed(steps_section, steps_body)
+
+
+def check_sections_indentation(steps_section):
+    steps_body = steps_section.get_section_body()
+    step_lvl1_first = steps_body.get_section_by_name("Failed step")
+    step_lvl1_second = steps_body.get_section_by_name("Partially success step")
+    assert step_lvl1_first.indent == step_lvl1_second.indent
+
+    step_lvl1_second.click()
+    step_lvl1_body = step_lvl1_second.get_section_body()
+    step_lvl2_first = step_lvl1_body.get_section_by_name("Success step")
+    step_lvl2_second = step_lvl1_body.get_section_by_name("Failed step")
+    assert step_lvl2_first.indent == step_lvl2_second.indent
+
+    assert steps_section.indent < step_lvl1_first.indent < step_lvl2_first.indent
+
+
+def check_step_collapsed(section, body):
+    assert section.is_section_collapsed
+    assert body.is_body_collapsed
+    assert not body.is_displayed()
+
+
+def check_step_not_collapsed(section, body):
+    assert not section.is_section_collapsed
+    assert not body.is_body_collapsed
+    assert body.is_displayed()
+
+
+class TestElement(FirefoxWebElement):
+
+    @staticmethod
+    def create(element):
+        assert(element)
+        element.__class__ = TestElement
+        return element;
+
+    # <input type="checkbox" id="1." class="hide">
+    # <label for="1.">  <-- returning this element
+    #     <span class="sectionLbl">1. Section name</span>  <-- Searching for this element
+    # </label>
+    # <div>Section body</div>
+    def get_section_by_name(self, section_name):
+        span_elements = self.find_elements_by_class_name("sectionLbl")
+        result = None
+        for el in span_elements:
+            if section_name in el.text:
+                result = el.find_element_by_xpath("..")
+        return TestElement.create(result)
+
+    def get_section_body(self):
+        body = TestElement.create(self.find_element_by_xpath("./following-sibling::*[1]"))
+        assert (body.tag_name == "div")
+        return body
+
+    @property
+    def is_body_collapsed(self):
+        display_state = self.value_of_css_property("display")
+        collapsed_state = "none"
+        not_collapsed_state = "block"
+        if display_state not in (collapsed_state, not_collapsed_state):
+            raise RuntimeError(f"Unexpected element display state: '{display_state}'")
+
+        collapsed = (display_state == collapsed_state)
+        collapsed &= (not self.text)
+        return collapsed
+
+    @property
+    def is_section_collapsed(self):
+        label_span = self.find_element_by_tag_name("span")
+        assert(label_span)
+        script = "return window.getComputedStyle(arguments[0],':before').getPropertyValue('content')"
+        section_state = self.parent.execute_script(script, label_span).replace('"', "").replace(" ", "");
+        collapsed_state = "[+]"
+        not_collapsed_state = "[-]"
+        if section_state not in (collapsed_state, not_collapsed_state):
+            raise RuntimeError(f"Unexpected section collapsed state: '{section_state}'")
+
+        return (section_state == collapsed_state)
+
+    @property
+    def indent(self):
+        return self.location['x']
