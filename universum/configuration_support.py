d42b89282c3e148066247bb33e8ed208bcbe9cff
miltolstoy <miltolstoy@gmail.com>
2022-08-04 10:59:22 +0300

    update Step class docstring

--- universum/configuration_support.py
+++ universum/configuration_support.py
@@ -61,8 +61,8 @@ class Step:
         one time at most.
     artifacts
         Path to the file or directory to be copied to the working directory as an execution
-        result. Can contain shell-style pattern matching (e.g. `"out/*.html"`), including recursive wildcards
-        (e.g. `"out/**/index.html"`). If not stated otherwise (see ``--no-archive``
+        result immediately after step finish. Can contain shell-style pattern matching (e.g. `"out/*.html"`),
+        including recursive wildcards (e.g. `"out/**/index.html"`). If not stated otherwise (see ``--no-archive``
         command-line parameter for details), artifact directories are copied
         as archives. If `artifact_prebuild_clean` key is either absent or set to `False`
         and stated artifacts are present in downloaded sources, it is considered a failure and configuration
