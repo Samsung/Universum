b41b36884a1161ecf9a56b7c97a9e3e4dc43e81d
dogbert911 <dogbert911@users.noreply.github.com>
2022-08-08 08:18:08 +0300

    fix

--- universum/modules/automation_server/github_server.py
+++ universum/modules/automation_server/github_server.py
@@ -46,4 +46,4 @@ class GithubServer(BaseServerForHostingBuild, BaseServerForTrigger, HasErrorStat
         return "Here is the link to Github Actions build: " + build_link
 
     def artifact_path(self, local_artifacts_dir: str, item: str) -> str:
-        raise RuntimeError("Artifact path function is not defined for current driver.")
+        return "/Artifact/not/exist"
