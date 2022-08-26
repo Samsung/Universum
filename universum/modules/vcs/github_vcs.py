f0595617765366e64e068b7dc97150c7d2039ba5
Ivan Keliukh <i.keliukh@samsung.com>
2022-08-26 18:13:06 +0300

    fix(pylint): warnings found by new pylint version

--- universum/modules/vcs/github_vcs.py
+++ universum/modules/vcs/github_vcs.py
@@ -211,7 +211,7 @@ class GithubMainVcs(ReportObserver, git_vcs.GitMainVcs, GithubTokenWithInstallat
             "Authorization": "token " + self.get_token()
         }
 
-        utils.make_request(check_url, request_method="PATCH", json=self.request, headers=headers)
+        utils.make_request(check_url, request_method="PATCH", json=self.request, headers=headers, timeout=5*60)
 
     def code_report_to_review(self, report):
         # git show returns string, each file separated by \n,
