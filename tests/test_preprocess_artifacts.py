aea2d78846e2560615624eb6fadc67f8a814935f
miltolstoy <miltolstoy@gmail.com>
2023-01-06 20:58:39 +0200

    test environment fixture

--- tests/test_preprocess_artifacts.py
+++ tests/test_preprocess_artifacts.py
@@ -1,87 +1,86 @@
 import inspect
 import os
+import pytest
 
 from universum import __main__
 
 
-def test_no_artifact_prebuild_clean(tmpdir):
-    config_file = write_config_file(tmpdir, artifact_prebuild_clean=True)
-    artifacts_dir = tmpdir.join("artifacts")
-
-    check_universum_executed_successfully(tmpdir, artifacts_dir, config_file)
-    check_step_artifact_present(artifacts_dir)
+@pytest.fixture()
+def test_env(tmpdir):
+    yield ArtifactsTestEnvironment(tmpdir)
 
 
-def test_no_artifact_no_prebuild_clean(tmpdir):
-    config_file = write_config_file(tmpdir, artifact_prebuild_clean=False)
-    artifacts_dir = tmpdir.join("artifacts")
-
-    check_universum_executed_successfully(tmpdir, artifacts_dir, config_file)
-    check_step_artifact_present(artifacts_dir)
+def test_no_artifact_prebuild_clean(test_env):
+    test_env.write_config_file(artifact_prebuild_clean=True)
+    check_universum_executed_successfully(test_env)
+    test_env.check_step_artifact_present()
 
 
-def test_existing_artifact_prebuild_clean(tmpdir):
-    config_file = write_config_file(tmpdir, artifact_prebuild_clean=True)
-    artifacts_dir = tmpdir.join("artifacts")
-
-    create_artifact_file(tmpdir)
-    check_universum_executed_successfully(tmpdir, artifacts_dir, config_file)
-    check_step_artifact_present(artifacts_dir)
+def test_no_artifact_no_prebuild_clean(test_env):
+    test_env.write_config_file(artifact_prebuild_clean=False)
+    check_universum_executed_successfully(test_env)
+    test_env.check_step_artifact_present()
 
 
-def test_existing_artifact_no_prebuild_clean(tmpdir):
-    config_file = write_config_file(tmpdir, artifact_prebuild_clean=False)
-    artifacts_dir = tmpdir.join("artifacts")
-
-    create_artifact_file(tmpdir)
-    check_universum_failed(tmpdir, artifacts_dir, config_file)
-    check_step_artifact_absent(artifacts_dir)
+def test_existing_artifact_prebuild_clean(test_env):
+    test_env.write_config_file(artifact_prebuild_clean=True)
+    test_env.create_artifact_file()
+    check_universum_executed_successfully(test_env)
+    test_env.check_step_artifact_present()
 
 
-def write_config_file(tmpdir, artifact_prebuild_clean):
-    config = inspect.cleandoc(f"""
-        from universum.configuration_support import Configuration, Step
-        step = Step(name='Step', command=['touch', 'artifact'], artifacts='artifact', 
-                    artifact_prebuild_clean={artifact_prebuild_clean})
-        configs = Configuration([step])
-    """)
-
-    config_file = tmpdir.join("configs.py")
-    config_file.write_text(config, "utf-8")
-
-    return config_file
+def test_existing_artifact_no_prebuild_clean(test_env):
+    test_env.write_config_file(artifact_prebuild_clean=False)
+    test_env.create_artifact_file()
+    check_universum_failed(test_env)
+    test_env.check_step_artifact_absent()
 
 
-def check_universum_executed_successfully(tmpdir, artifacts_dir, config_file):
-    return_code = launch_universum(tmpdir, artifacts_dir, config_file)
+def check_universum_executed_successfully(test_env):
+    return_code = launch_universum(test_env)
     assert return_code == 0
 
 
-def check_universum_failed(tmpdir, artifacts_dir, config_file):
-    return_code = launch_universum(tmpdir, artifacts_dir, config_file)
+def check_universum_failed(test_env):
+    return_code = launch_universum(test_env)
     assert return_code == 1
 
 
-def launch_universum(tmpdir, artifacts_dir, config_file):
+def launch_universum(test_env):
     params = ["-vt", "none",
-              "-fsd", str(tmpdir),
-              "-ad", str(artifacts_dir),
+              "-fsd", str(test_env.tmpdir),
+              "-ad", str(test_env.artifacts_dir),
               "--clean-build",
               "-o", "console",
-              "-cfg", str(config_file)]
+              "-cfg", str(test_env.config_file)]
     return __main__.main(params)
 
 
-def check_step_artifact_present(artifacts_dir):
-    step_artifact = artifacts_dir.join("artifact")
-    assert os.path.exists(step_artifact)
+class ArtifactsTestEnvironment:
 
+    def __init__(self, tmpdir):
+        self.tmpdir = tmpdir
+        self.artifacts_dir = tmpdir.join("artifacts")
+        self.artifact_name = "artifact"
+        self.artifact_path = self.artifacts_dir.join(self.artifact_name)
+        self.config_file = None
 
-def check_step_artifact_absent(artifacts_dir):
-    step_artifact = artifacts_dir.join("artifact")
-    assert not os.path.exists(step_artifact)
+    def write_config_file(self, artifact_prebuild_clean):
+        config = inspect.cleandoc(f"""
+            from universum.configuration_support import Configuration, Step
+            step = Step(name='Step', command=['touch', '{self.artifact_name}'], artifacts='{self.artifact_name}', 
+                        artifact_prebuild_clean={artifact_prebuild_clean})
+            configs = Configuration([step])
+        """)
+        self.config_file = self.tmpdir.join("configs.py")
+        self.config_file.write_text(config, "utf-8")
 
+    def check_step_artifact_present(self):
+        assert os.path.exists(self.artifact_path)
 
-def create_artifact_file(tmpdir):
-    artifact_file = tmpdir.join("artifact")
-    open(artifact_file, "w").close()
+    def check_step_artifact_absent(self):
+        assert not os.path.exists(self.artifact_path)
+
+    def create_artifact_file(self):
+        artifact_path = self.tmpdir.join(self.artifact_name)
+        open(artifact_path, "w").close()
