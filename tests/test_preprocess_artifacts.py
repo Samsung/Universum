faeab4d288b6f11b135e419006b1efc07954323d
miltolstoy <miltolstoy@gmail.com>
2023-01-19 07:47:04 +0200

    add typing

--- tests/test_preprocess_artifacts.py
+++ tests/test_preprocess_artifacts.py
@@ -1,52 +1,23 @@
 import inspect
 import os
+import pathlib
+
 import pytest
 
 from .utils import LocalTestEnvironment
-
-
-@pytest.fixture()
-def test_env(tmpdir):
-    yield ArtifactsTestEnvironment(tmpdir)
-
-
-def test_no_artifact_prebuild_clean(test_env):
-    test_env.write_config_file(artifact_prebuild_clean=True)
-    test_env.run()
-    test_env.check_step_artifact_present()
-
-
-def test_no_artifact_no_prebuild_clean(test_env):
-    test_env.write_config_file(artifact_prebuild_clean=False)
-    test_env.run()
-    test_env.check_step_artifact_present()
-
-
-def test_existing_artifact_prebuild_clean(test_env):
-    test_env.write_config_file(artifact_prebuild_clean=True)
-    test_env.create_artifact_file()
-    test_env.run()
-    test_env.check_step_artifact_present()
-
-
-def test_existing_artifact_no_prebuild_clean(test_env, stdout_checker):
-    test_env.write_config_file(artifact_prebuild_clean=False)
-    test_env.create_artifact_file()
-    test_env.run(expect_failure=True)
-    stdout_checker.assert_has_calls_with_param(f"already exist in '/.*' directory", is_regexp=True)
-    test_env.check_step_artifact_absent()
+from .conftest import FuzzyCallChecker
 
 
 class ArtifactsTestEnvironment(LocalTestEnvironment):
 
-    def __init__(self, tmpdir):
+    def __init__(self, tmpdir: pathlib.Path):
         super().__init__(tmpdir, "main")
-        self.artifact_name = "artifact"
-        self.artifact_path = self.artifact_dir.join(self.artifact_name)
-        self.artifact_content = "artifact content"
+        self.artifact_name: str = "artifact"
+        self.artifact_path: pathlib.Path = self.artifact_dir.join(self.artifact_name)
+        self.artifact_content: str = "artifact content"
 
-    def write_config_file(self, artifact_prebuild_clean):
-        config = inspect.cleandoc(f"""
+    def write_config_file(self, artifact_prebuild_clean: bool):
+        config: str = inspect.cleandoc(f"""
             from universum.configuration_support import Configuration, Step
             step = Step(name='Step', 
                         command=['bash', '-c', 'echo "{self.artifact_content}" > {self.artifact_name}'], 
@@ -59,13 +30,45 @@ class ArtifactsTestEnvironment(LocalTestEnvironment):
     def check_step_artifact_present(self):
         assert os.path.exists(self.artifact_path)
         with open(self.artifact_path) as f:
-            content = f.read().replace("\n", "")
+            content: str = f.read().replace("\n", "")
             assert content == self.artifact_content
 
     def check_step_artifact_absent(self):
         assert not os.path.exists(self.artifact_path)
 
     def create_artifact_file(self):
-        precreated_artifact = self.src_dir.join(self.artifact_name)
+        precreated_artifact: pathlib.Path = self.src_dir.join(self.artifact_name)
         with open(precreated_artifact, "w") as f:
             f.write("pre-created artifact content")
+
+
+@pytest.fixture()
+def test_env(tmpdir: pathlib.Path):
+    yield ArtifactsTestEnvironment(tmpdir)
+
+
+def test_no_artifact_prebuild_clean(test_env: ArtifactsTestEnvironment):
+    test_env.write_config_file(artifact_prebuild_clean=True)
+    test_env.run()
+    test_env.check_step_artifact_present()
+
+
+def test_no_artifact_no_prebuild_clean(test_env: ArtifactsTestEnvironment):
+    test_env.write_config_file(artifact_prebuild_clean=False)
+    test_env.run()
+    test_env.check_step_artifact_present()
+
+
+def test_existing_artifact_prebuild_clean(test_env: ArtifactsTestEnvironment):
+    test_env.write_config_file(artifact_prebuild_clean=True)
+    test_env.create_artifact_file()
+    test_env.run()
+    test_env.check_step_artifact_present()
+
+
+def test_existing_artifact_no_prebuild_clean(test_env: ArtifactsTestEnvironment, stdout_checker: FuzzyCallChecker):
+    test_env.write_config_file(artifact_prebuild_clean=False)
+    test_env.create_artifact_file()
+    test_env.run(expect_failure=True)
+    stdout_checker.assert_has_calls_with_param(f"already exist in '/.*' directory", is_regexp=True)
+    test_env.check_step_artifact_absent()
