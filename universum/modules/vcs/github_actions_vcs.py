0787471f810dcc52734e003fd831f31ca88b2fc9
k-dovgan <k.dovgan@samsung.com>
2023-06-02 16:13:32 +0300

    fix(vcs): add typing to newly added file

--- universum/modules/vcs/github_actions_vcs.py
+++ universum/modules/vcs/github_actions_vcs.py
@@ -1,10 +1,13 @@
 import json
 import urllib.parse
+from typing import Dict, List, Union, Optional
+from typing_extensions import Self
 
 from . import git_vcs
 from ..reporter import ReportObserver, Reporter
 from ...lib import utils
 from ...lib.gravity import Dependency
+from ...lib.module_arguments import ModuleArgumentParser
 
 __all__ = [
     "GithubActionsMainVcs"
@@ -15,21 +18,22 @@ class GithubActionsMainVcs(ReportObserver, git_vcs.GitMainVcs):
     """
     This class mostly contains functions for GitHub report observer
     """
-    reporter_factory = Dependency(Reporter)
+    reporter_factory: Dependency = Dependency(Reporter)
 
     @staticmethod
-    def define_arguments(argument_parser):
-        parser = argument_parser.get_or_create_group("GitHub Actions", "GitHub repository settings for GH Actions")
+    def define_arguments(argument_parser: ModuleArgumentParser) -> None:
+        parser: ModuleArgumentParser = argument_parser.get_or_create_group("GitHub Actions",
+                                                                           "GitHub repository settings for GH Actions")
 
         parser.add_argument("--ghactions-token", "-ght", dest="token", metavar="GITHUB_TOKEN",
                             help="Is stored in ${{ secrets.GITHUB_TOKEN }}")
         parser.add_argument("--ghactions-payload", "-ghp", dest="payload", metavar="GITHUB_PAYLOAD",
                             help="File path: ${{ github.event_path }}")
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         self.settings.repo = "Will be filled after payload parsing"
         super().__init__(*args, **kwargs)
-        self.reporter = None
+        self.reporter: Reporter = None  # type: ignore
 
         self.check_required_option("token", """
             The GitHub workflow token is not specified.
@@ -42,7 +46,7 @@ class GithubActionsMainVcs(ReportObserver, git_vcs.GitMainVcs):
             from the webhook payload and passed via GIT_CHECKOUT_ID environment variable.
             """)
 
-        self.payload = self.read_and_check_multiline_option("payload", """
+        self.payload: str = self.read_and_check_multiline_option("payload", """
             GitHub web-hook payload JSON is not specified.
 
             Please pass incoming web-hook request payload to this parameter directly via
@@ -54,47 +58,49 @@ class GithubActionsMainVcs(ReportObserver, git_vcs.GitMainVcs):
             """)
 
         try:
-            self.payload_json = json.loads(self.payload)
+            self.payload_json: dict = json.loads(self.payload)
             self.settings.repo = self.payload_json['repository']['html_url']
             self.settings.refspec = self.payload_json['pull_request']['head']['ref']
         except json.decoder.JSONDecodeError as error:
             self.error(f"Provided payload value could not been parsed as JSON "
                        f"and returned the following error:\n {error}")
 
-    def _clone(self, history_depth, destination_directory, clone_url):
-        parsed_repo = urllib.parse.urlsplit(clone_url)
+    def _clone(self, history_depth: int, destination_directory: str, clone_url: str) -> None:
+        parsed_repo: urllib.parse.SplitResult = urllib.parse.urlsplit(clone_url)
         if parsed_repo.scheme == "https" and not parsed_repo.username:
             new_netloc = f"{self.settings.token}@{parsed_repo.netloc}"
-            parsed_repo = (parsed_repo.scheme, new_netloc, parsed_repo.path, parsed_repo.query, parsed_repo.fragment)
-        clone_url = urllib.parse.urlunsplit(parsed_repo)
+            parsed_repo = (parsed_repo.scheme, new_netloc, parsed_repo.path,
+                           parsed_repo.query, parsed_repo.fragment)  # type: ignore
+        clone_url = str(urllib.parse.urlunsplit(parsed_repo))
         super()._clone(history_depth, destination_directory, clone_url)
 
-    def code_review(self):
+    def code_review(self) -> Self:
         self.reporter = self.reporter_factory()
         self.reporter.subscribe(self)
         return self
 
-    def update_review_version(self):
+    def update_review_version(self) -> None:
         self.out.log("GitHub has no review versions")
 
-    def get_review_link(self):
+    def get_review_link(self) -> str:
         return self.payload_json['pull_request']['html_url']
 
-    def is_latest_version(self):
+    def is_latest_version(self) -> bool:
         return True
 
-    def _report(self, url, request):
-        headers = {
+    def _report(self, url, request: dict) -> None:
+        headers: Dict = {
             "Accept": "application/vnd.github+json",
             "Authorization": f"Bearer { self.settings.token }"
         }
 
         utils.make_request(url, request_method="POST", json=request, headers=headers, timeout=5*60)
 
-    def code_report_to_review(self, report):
+    def code_report_to_review(self, report: dict) -> None:
         # git show returns string, each file separated by \n,
         # first line consists of commit id and commit comment, so it's skipped
-        commit_files = self.repo.git.show("--name-only", "--oneline", self.settings.checkout_id).split('\n')[1:]
+        commit_files: List[str] = self.repo.git.show("--name-only", "--oneline",
+                                                     self.settings.checkout_id).split('\n')[1:]
         # NB! When using GITHUB_TOKEN, the rate limit is 1,000 requests per hour per repository.
         # (https://docs.github.com/en/rest/overview/resources-in-the-rest-api?apiVersion=2022-11-28  ->
         #                                              #rate-limits-for-requests-from-github-actions)
@@ -109,10 +115,10 @@ class GithubActionsMainVcs(ReportObserver, git_vcs.GitMainVcs):
                                side="RIGHT")
                 self._report(self.payload_json['pull_request']['review_comments_url'], request)
 
-    def report_start(self, report_text):
+    def report_start(self, report_text: str) -> None:
         pass
 
-    def report_result(self, result, report_text=None, no_vote=False):
+    def report_result(self, result: bool, report_text: Optional[str] = None, no_vote: bool = False):
         if not report_text:
             report_text = "Universum check finished"
         self._report(self.payload_json['pull_request']['comments_url'], {"body": report_text})
